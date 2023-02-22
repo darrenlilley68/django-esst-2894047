1b3c17b1681564d9b54f1ddccb288b6b3ed71c97
Darren Lilley <darren.lilley68@gmail.com>
2023-02-22 17:34:04 +0000

    3.3 Using Admin for data creation and manipulation

--- home/models.py
+++ home/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-class notes(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+
