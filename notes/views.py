59be48f1efef10668c18cb075d2c16c961661930
Darren Lilley <darren.lilley68@gmail.com>
2023-02-27 16:46:49 +0000

    8.3 Adding a new note after ForeignKey

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -16,10 +16,18 @@ class NotesUpdateView(UpdateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+        
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
